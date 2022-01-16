4edc37ee9a805a082fbd8f283593040b12be88b0
Robson Manoel Bento Teixeira <robsonmteixeira@outlook.com>
2022-01-16 14:08:36 -0300

    Commit Inicial - P2

---
+++ P2/Domain/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P2.Domain
+{
+    public class User
+    {
+        public User()
+        {
+            Roles = new List<Role>();
+        }
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid EmailId { get; set; }
+
+        public virtual Email Email { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime LastUpdateDate { get; set; }
+
+        public string LastUpdateUser { get; set; }
+
+        public virtual ICollection<Role> Roles { get; set; }
+    }
+}
---
+++ P2/Program.cs
@@ -0,0 +1,91 @@
+﻿using P2.DataContext;
+using P2.Domain;
+using System;
+using System.Linq;
+
+namespace P2
+{
+    static class Program
+    {
+        static void Main()
+        {
+            MyDataContext myDataContext = new MyDataContext();
+
+            #region Insert
+
+            #region E-mail
+
+            Email email = new Email
+            {
+                Name = "treinamentos",
+                Domain = "@andrebaltieri.net",
+                IsActive = true,
+                LastUpdateDate = DateTime.Now,
+                LastUpdateUser = "master"
+            };
+
+            myDataContext.Emails.Add(email);
+
+            #endregion
+
+            #region Organization
+
+            Organization organization = new Organization
+            {
+                Name = "André Baltieri Treinamentos",
+                Email = email,
+                Image = "imagemTeste",
+                IsActive = true,
+                LastUpdateDate = DateTime.Now,
+                LastUpdateUser = "master"
+            };
+
+            myDataContext.Organizations.Add(organization);
+
+            #endregion
+
+            #region Franchise
+
+            Franchise franchise = new Franchise
+            {
+                Name = "Piracicaba",
+                OrganizationId = organization.Id,
+                IsActive = true,
+                LastUpdateDate = DateTime.Now,
+                LastUpdateUser = "master"
+            };
+
+            myDataContext.Franchises.Add(franchise); 
+            
+            #endregion
+
+            myDataContext.SaveChanges();
+
+            #endregion
+
+            #region Select
+
+            Organization organizationRetorno;
+
+            organizationRetorno = myDataContext.Organizations
+                .Find(organization.Id);
+
+            if (organizationRetorno != null)
+                Console.WriteLine($"Consulta via Find.");
+
+            organizationRetorno = myDataContext.Organizations
+                .Where(x => x.Id == organization.Id && x.IsActive)
+                .FirstOrDefault();
+
+            if (organizationRetorno != null)
+                Console.WriteLine($"Consulta via Linq.");
+
+            #endregion
+
+            myDataContext.Dispose();
+
+            Console.WriteLine("Concluído!");
+            Console.ReadKey();
+        }
+    }
+}
